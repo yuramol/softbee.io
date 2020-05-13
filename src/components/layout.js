9e779fcbae6182123468bd60e5e9f9f12b10e90a
yuramol <yuramol86@gmail.com>
2020-05-14 00:37:39 +0300

    start creating footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,26 +3,21 @@ import styled from "styled-components"
 
 class Layout extends React.Component {
   render() {
-    const {  children } = this.props
-  
+    const { children } = this.props
+
     return (
-        <Wrapper>
-          <div
-            style={{
-              marginLeft: `auto`,
-              marginRight: `auto`,
-              maxWidth: '900px',
-            }}
-          >
-            <header/>
-            <main>{children}</main>
-          </div>
-          <Footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </Footer>
-        </Wrapper>
+      <Wrapper>
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: "900px",
+          }}
+        >
+          <header />
+          <main>{children}</main>
+        </div>        
+      </Wrapper>
     )
   }
 }
@@ -30,10 +25,4 @@ class Layout extends React.Component {
 const Wrapper = styled.div`
   min-height: 100vh;
 `
-
-const Footer = styled.footer`
-  text-align: center;
-  margin: 24px;
-`
-
 export default Layout
