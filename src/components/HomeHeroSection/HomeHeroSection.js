13a799d099d99dab4290fcd1e21182480b558116
Omelchenko Oleksandr <pro4esor@gmail.com>
2020-05-18 11:18:41 +0300

    Edits path to image

--- src/components/HomeHeroSection/HomeHeroSection.js
+++ src/components/HomeHeroSection/HomeHeroSection.js
@@ -1,53 +1,42 @@
-import React from "react"
+import React from 'react';
 import { Grommet, Box, Button, Grid, Text } from 'grommet';
 
-import { theme } from "../../utils/theme";
-import { Heading } from "../../legos/typography/Heading";
-import { StyledButton } from "../../legos/Button/Button"
+import { theme } from '../../utils/theme';
+import { Heading } from '../../legos/typography/Heading';
+import { StyledButton } from '../../legos/Button/Button';
 
 class HomeHeroSection extends React.Component {
   render() {
-
     return (
       <Grommet theme={theme}>
-          <Grid
-             columns={{count: 2,size: "auto"}}
-              gap="small"
-              pad="xlarge"
-          >
-          <Box justify="center" align="start" >
-              <Box pad={{"bottom": "small"}}>
+        <Grid columns={{ count: 2, size: 'auto' }} gap="small" pad="xlarge">
+          <Box justify="center" align="start">
+            <Box pad={{ bottom: 'small' }}>
               <Heading level={1} color="#104065" wordBreak="normal">
-                Your partners in
-                new products creating
+                Your partners in new products creating
               </Heading>
-              </Box>
-              <Box 
-              width="300px"
-              pad={{"bottom": "large"}}
-              >
-                <Text color="#104065" style={{ lineHeight: "32px" }}>
-                  with a bear drinking afterwards.
-                  UPDATED: and staying at home =)
-                </Text>
-              </Box>
-         
-            <Box 
-             justify="center"
-             align="center"
-             height="60px"
-             width="200px">
-            <StyledButton label="Let’s talk"  fill color="#F5D040"  primary/>
             </Box>
+            <Box width="300px" pad={{ bottom: 'large' }}>
+              <Text color="#104065" style={{ lineHeight: '32px' }}>
+                with a bear drinking afterwards. UPDATED: and staying at home =)
+              </Text>
             </Box>
-            <Box align="center">
-              <img style={{}} src="./assets/sectioHeader.svg" alt="Gatsby Scene" />
-            </Box>
-          </Grid>
-      </Grommet>
 
-    )
+            <Box justify="center" align="center" height="60px" width="200px">
+              <StyledButton label="Let’s talk" fill color="#F5D040" primary />
+            </Box>
+          </Box>
+          <Box align="center">
+            <img
+              style={{}}
+              src="./assets/sectioHeader.svg"
+              alt="Gatsby Scene"
+            />
+          </Box>
+        </Grid>
+      </Grommet>
+    );
   }
 }
 
-export default HomeHeroSection
+export default HomeHeroSection;
