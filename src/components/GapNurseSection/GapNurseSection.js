111efa6c3d8b3ae52aff291906aa94edfb9b0b98
Omelchenko Oleksandr <pro4esor@gmail.com>
2020-05-19 15:31:24 +0300

    Remove <Gorommet> wrapper in all sections

--- src/components/GapNurseSection/GapNurseSection.js
+++ src/components/GapNurseSection/GapNurseSection.js
@@ -1,50 +1,44 @@
 import React from 'react';
-import { Grommet, Box, Button, Grid } from 'grommet';
+import { Box, Button, Grid } from 'grommet';
 
 import { Text } from '../../legos/typography/Text';
 import { Heading } from '../../legos/typography/Heading';
 
-import { theme } from '../../utils/theme';
 
 class GapNurseSection extends React.Component {
   render() {
     return (
-      <Grommet theme={theme}>
-        <Box background={{ color: '#F0F6F4' }}>
-          <Grid columns={{ count: 2, size: 'auto' }} gap="small" pad="xlarge">
-            <Box justify="center" align="start">
-              <Box width="470px" pad={{ bottom: 'small' }}>
-                <Heading level={2} color="#104065" wordBreak="normal">
-                  They really help to fight COVID-19 in US
-                </Heading>
-              </Box>
-               <Box width="380px" pad={{ bottom: 'medium' }}>
-                <Text
-                  size="medium"
-                  color="#104065"
-                  style={{ fontFamily: 'HelveticaNeueCyr' }}
-                >
-                  With GapNurse, facilities know they’re covered during critical
-                  moments and unexpected absences without breaking your budget.
-                </Text>
-              </Box>
-              <Button
-                style={{ textDecorationLine: 'underline' }}
-                plain
-                label="See case study"
-                color="#25BBC5"
-                onClick={() => {}}
-              />
+      <Box background={{ color: '#F0F6F4' }}>
+        <Grid columns={{ count: 2, size: 'auto' }} gap="small" pad="xlarge">
+          <Box justify="center" align="start">
+            <Box width="470px" pad={{ bottom: 'small' }}>
+              <Heading level={2} color="#104065">
+                They really help to fight COVID-19 in US
+              </Heading>
             </Box>
-            <Box align="center">
-              <img
-                src="./assets/phoneGapnurse.svg"
-                alt="Gatsby Scene"
-              />
+            <Box width="380px" pad={{ bottom: 'medium' }}>
+              <Text
+                size="medium"
+                color="#104065"
+                style={{ fontFamily: 'HelveticaNeueCyr' }}
+              >
+                With GapNurse, facilities know they’re covered during critical
+                moments and unexpected absences without breaking your budget.
+              </Text>
             </Box>
-          </Grid>
-        </Box>
-      </Grommet>
+            <Button
+              style={{ textDecorationLine: 'underline' }}
+              plain
+              label="See case study"
+              color="#25BBC5"
+              onClick={() => {}}
+            />
+          </Box>
+          <Box align="center">
+            <img src="./assets/phoneGapnurse.svg" alt="Gatsby Scene" />
+          </Box>
+        </Grid>
+      </Box>
     );
   }
 }
--- src/components/MMOSection/MMOSection.js
+++ src/components/MMOSection/MMOSection.js
@@ -1,65 +1,56 @@
 import React from 'react';
-import { Grommet, Box, Button, Grid } from 'grommet';
+import { Box, Button, Grid } from 'grommet';
 
-import { theme } from '../../utils/theme';
 import { Heading } from '../../legos/typography/Heading';
 import { Text } from '../../legos/typography/Text';
 
 class MMOSection extends React.Component {
   render() {
     return (
-      <Grommet theme={theme}>
-        <Box
-          height="800px"
-          justify="center"
-          background={{
-            size: 'large',
-            position: 'absolute',
-            image: 'url(./assets/mmoBackground.svg)',
-          }}
-        >
-          <Grid columns={['2/3', '1/4']} pad="xlarge">
-            <Box direction="row" justify="center">
-              <Box justify="center" align="start" margin={{ left: 'large' }}>
-                <img
-                  src="./assets/sectionMMO.svg"
-                  alt="Gatsby Scene"
-                />
-              </Box>
-              <Box justify="center" pad={{ left: 'xlarge', right: 'small' }}>
-                <img 
-                  src="./assets/mmoSectionMini.svg"
-                  alt="Gatsby Scene"
-                />
-              </Box>
+      <Box
+        height="800px"
+        justify="center"
+        background={{
+          size: 'large',
+          position: 'absolute',
+          image: 'url(./assets/mmoBackground.svg)',
+        }}
+      >
+        <Grid columns={['2/3', '1/4']} pad="xlarge">
+          <Box direction="row" justify="center">
+            <Box justify="center" align="start" margin={{ left: 'large' }}>
+              <img src="./assets/sectionMMO.svg" alt="Gatsby Scene" />
             </Box>
-            <Box justify="center" align="start" margin={{ right: 'large' }}>
-              <Box>
-                <Heading level={2} color="#104065">
-                  MMO.cat selling website
-                </Heading>
-              </Box>
-              <Box width="491px" pad={{ bottom: 'small' }}>
-                <Box pad={{ bottom: 'large' }}>
-                  <Text size="medium" color="#104065">
-                    We provided full-stack development service
-                  </Text>
-                  <Text size="medium" color="#104065">
-                    for end-to-end websites of MMO.cat team
-                  </Text>
-                </Box>
-                <Button
-                  style={{ textDecorationLine: 'underline' }}
-                  plain
-                  label="See case study"
-                  color="#25BBC5"
-                  onClick={() => {}}
-                />
-              </Box>
+            <Box justify="center" pad={{ left: 'xlarge', right: 'small' }}>
+              <img src="./assets/mmoSectionMini.svg" alt="Gatsby Scene" />
             </Box>
-          </Grid>
-        </Box>
-      </Grommet>
+          </Box>
+          <Box justify="center" align="start" margin={{ right: 'large' }}>
+            <Box>
+              <Heading level={2} color="#104065">
+                MMO.cat selling website
+              </Heading>
+            </Box>
+            <Box width="491px" pad={{ bottom: 'small' }}>
+              <Box pad={{ bottom: 'large' }}>
+                <Text size="medium" color="#104065">
+                  We provided full-stack development service
+                </Text>
+                <Text size="medium" color="#104065">
+                  for end-to-end websites of MMO.cat team
+                </Text>
+              </Box>
+              <Button
+                style={{ textDecorationLine: 'underline' }}
+                plain
+                label="See case study"
+                color="#25BBC5"
+                onClick={() => {}}
+              />
+            </Box>
+          </Box>
+        </Grid>
+      </Box>
     );
   }
 }
