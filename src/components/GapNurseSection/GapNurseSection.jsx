5ffcf4f381996c06e11783ad7c85c1b57365382c
Artem Marochkanych <serartmar@gmail.com>
2020-06-09 11:40:56 +0300

    Implement an example of responsiveness with ResponsiveContext

--- src/components/GapNurseSection/GapNurseSection.jsx
+++ src/components/GapNurseSection/GapNurseSection.jsx
@@ -1,34 +1,45 @@
 import React from 'react';
-import { Box, Grid } from 'grommet';
+import { Box, Grid, ResponsiveContext } from 'grommet';
 
 import { Text } from '../../legos/typography/Text';
 import { Heading } from '../../legos/typography/Heading';
 import { RouterLink } from '../../legos/RouterLink';
 
-export const GapNurseSection = () => (
-  <Box background={{ color: '#F0F6F4' }}>
-    <Grid columns={{ count: 2, size: 'auto' }} gap="small" pad="xlarge">
-      <Box justify="center" align="start">
-        <Box width="470px" pad={{ bottom: 'small' }}>
-          <Heading level={2} color="brand">
-            They really help to fight COVID-19 in US
-          </Heading>
+export const GapNurseSection = () => {
+  const size = React.useContext(ResponsiveContext);
+  const columnsCount = size === 'small' ? 1 : 2;
+  const rowsCount = size === 'small' ? 2 : 1;
+
+  return (
+    <Box background={{ color: '#F0F6F4' }}>
+      <Grid
+        columns={{ count: columnsCount, size: 'auto' }}
+        rows={{ count: rowsCount, size: 'auto' }}
+        gap="small"
+        pad="xlarge"
+      >
+        <Box justify="center" align="start">
+          <Box width="470px" pad={{ bottom: 'small' }}>
+            <Heading level={2} color="brand">
+              They really help to fight COVID-19 in US
+            </Heading>
+          </Box>
+          <Box width="380px" pad={{ bottom: 'medium' }}>
+            <Text
+              size="medium"
+              color="text-dark-grey"
+              style={{ fontFamily: 'HelveticaNeueCyr' }}
+            >
+              GapNurse, facilities know they’re covered during critical moments
+              and unexpected absences without breaking your budget.
+            </Text>
+          </Box>
+          <RouterLink to="gapnurse-case-study">See case study</RouterLink>
         </Box>
-        <Box width="380px" pad={{ bottom: 'medium' }}>
-          <Text
-            size="medium"
-            color="text-dark-grey"
-            style={{ fontFamily: 'HelveticaNeueCyr' }}
-          >
-            GapNurse, facilities know they’re covered during critical moments
-            and unexpected absences without breaking your budget.
-          </Text>
+        <Box align="center">
+          <img src="./assets/phoneGapnurse.svg" alt="Gatsby Scene" />
         </Box>
-        <RouterLink to="gapnurse-case-study">See case study</RouterLink>
-      </Box>
-      <Box align="center">
-        <img src="./assets/phoneGapnurse.svg" alt="Gatsby Scene" />
-      </Box>
-    </Grid>
-  </Box>
-);
+      </Grid>
+    </Box>
+  );
+};
--- src/components/HomeHeroSection/HomeHeroSection.jsx
+++ src/components/HomeHeroSection/HomeHeroSection.jsx
@@ -1,38 +1,48 @@
 import React from 'react';
-import { Box, Grid, Text } from 'grommet';
+import { Box, Grid, Text, ResponsiveContext } from 'grommet';
 
 import { Heading } from '../../legos/typography/Heading';
 import { Button } from '../../legos/Button/Button';
 
-export const HomeHeroSection = () => (
-  <Grid columns={{ count: 2, size: 'auto' }} gap="small" pad="xlarge">
-    <Box justify="center" align="start">
-      <Box pad={{ bottom: 'small' }}>
-        <Heading level={1} color="brand">
-          Your partners in new products creating
-        </Heading>
-      </Box>
-      <Box width="300px" pad={{ bottom: 'large' }}>
-        <Text color="brand" style={{ lineHeight: '32px' }}>
-          with a bear drinking afterwards.
-        </Text>
-        <Text color="brand" style={{ lineHeight: '32px' }}>
-          UPDATED: and staying at home =)
-        </Text>
-      </Box>
+export const HomeHeroSection = () => {
+  const size = React.useContext(ResponsiveContext);
+  const columnsCount = size === 'small' ? 1 : 2;
+  const rowsCount = size === 'small' ? 2 : 1;
+  return (
+    <Grid
+      columns={{ count: columnsCount, size: 'auto' }}
+      rows={{ count: rowsCount, size: 'auto' }}
+      gap="small"
+      pad="xlarge"
+    >
+      <Box justify="center" align="start">
+        <Box pad={{ bottom: 'small' }}>
+          <Heading level={1} color="brand">
+            Your partners in new products creating
+          </Heading>
+        </Box>
+        <Box width="300px" pad={{ bottom: 'large' }}>
+          <Text color="brand" style={{ lineHeight: '32px' }}>
+            with a bear drinking afterwards.
+          </Text>
+          <Text color="brand" style={{ lineHeight: '32px' }}>
+            UPDATED: and staying at home =)
+          </Text>
+        </Box>
 
-      <Box justify="center" align="center" height="60px" width="200px">
-        <Button
-          label="Let’s talk"
-          fill
-          color="accent-1"
-          primary
-          href="/let's-talk"
-        />
+        <Box justify="center" align="center" height="60px" width="200px">
+          <Button
+            label="Let’s talk"
+            fill
+            color="accent-1"
+            primary
+            href="/let's-talk"
+          />
+        </Box>
       </Box>
-    </Box>
-    <Box align="center">
-      <img src="./assets/sectionHeader.svg" alt="Gatsby Scene" />
-    </Box>
-  </Grid>
-);
+      <Box align="center">
+        <img src="./assets/sectionHeader.svg" alt="Gatsby Scene" />
+      </Box>
+    </Grid>
+  );
+};
