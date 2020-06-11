314ecad1ab83f163c8127bf4ca17b5c1e1972d97
Omelchenko Oleksandr <pro4esor@gmail.com>
2020-06-11 11:35:57 +0300

    Add react-responsive package

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,10 +5,10 @@ import { SEO } from '../components/SEO';
 import { HomeHeroSection } from '../components/HomeHeroSection/HomeHeroSection';
 import { GapNurseSection } from '../components/GapNurseSection/GapNurseSection';
 import { TeamSection } from '../components/TeamSection/TeamSection';
-import { MMOSection } from '../components/MMOSection/MMOSection';
-import { LookingSection } from '../components/LookingSection/LookingSection';
-import { WhatWeDoSection } from '../components/WhatWeDoSection/WhatWeDoSection';
-import { LetsStarted } from '../components/LetsStarted/LetsStarted';
+// import { MMOSection } from '../components/MMOSection/MMOSection';
+// import { LookingSection } from '../components/LookingSection/LookingSection';
+// import { WhatWeDoSection } from '../components/WhatWeDoSection/WhatWeDoSection';
+// import { LetsStarted } from '../components/LetsStarted/LetsStarted';
 
 const siteTitle = 'SoftBee';
 const keywords = [
@@ -26,10 +26,10 @@ const IndexPage = ({ location }) => {
       <HomeHeroSection />
       <GapNurseSection />
       <TeamSection />
-      <MMOSection />
+      {/* <MMOSection />
       <LookingSection />
       <WhatWeDoSection />
-      <LetsStarted />
+      <LetsStarted /> */}
     </Layout>
   );
 };
