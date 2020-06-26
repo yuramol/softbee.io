14940de98cddcd4bd397b0cb6401ac4d107aa790
Oleg Duma <dumik.alive@gmail.com>
2020-06-26 18:18:47 +0300

    Finished responsive

--- src/components/CaseGapNurse/CaseGapNurse.jsx
+++ src/components/CaseGapNurse/CaseGapNurse.jsx
@@ -10,74 +10,73 @@ import { ButtonNextCase } from '../ButtonNextCase/ButtonNextCase';
 export const CaseGapNurse = () => {
   const size = React.useContext(ResponsiveContext);
   const columnsCount = size === 'small' ? 1 : 2;
-  const columnsCounts = size === 'small' ? 1 : 2;
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1010px)' });
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1110px)' });
+  const columnsCountTablet = isTabletOrMobile ? 1 : 2;
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   const textAlignVariant = isMobile ? 'center' : 'start';
   return (
     <Box>
       <Box
-        height="830px"
-        background={{
-          position: 'bottom',
-          size: 'small',
-          image: 'url(../assets/backgroundCaseGapnurse.svg)',
-        }}
+        style={isMobile ? { height: '1057px' } : { height: '830px' }}
+        background={
+          isMobile
+            ? undefined
+            : {
+                position: 'bottom',
+                size: 'small',
+                image: 'url(../assets/backgroundCaseGapnurse.svg)',
+              }
+        }
       >
         <Box width="100%" align="center">
-          <Heading size="medium" level={2} color="brand">
+          <Heading level={2} color="brand">
             Mobile app
           </Heading>
         </Box>
         <Grid
           columns={{ count: columnsCount, size: ['auto', 'auto'] }}
           pad={{ horizontal: 'xlarge' }}
-          style={{ height: '480px' }}
+          style={isMobile ? { height: '800px' } : { height: '480px' }}
           align={textAlignVariant}
         >
-          <Box
-            justify="start"
-            align={textAlignVariant}
-            pad={{ left: 'large', top: 'large' }}
-          >
+          <Box align={textAlignVariant} pad={{ left: 'large', top: 'large' }}>
             <Box>
               <img src="../assets/logoGapNurse.svg" alt=" Gap Nurse " />
             </Box>
-            <Box
-              width="470px"
-              size="small"
-              pad={
-                isTabletOrMobile
-                  ? { top: 'small' }
-                  : { bottom: 'xxsmall', top: 'xlarge' }
-              }
-            >
-              <Heading
-                size="small"
-                level={4}
-                color="brand"
-                style={{ lineHeight: '130%' }}
-                pad="xlarge"
-              >
-                About client:
-              </Heading>
-            </Box>
+            {isMobile || (
+              <Box>
+                <Box width="470px" pad={{ bottom: 'xxsmall', top: 'xlarge' }}>
+                  <Heading
+                    level={4}
+                    color="brand"
+                    style={{ lineHeight: '130%' }}
+                  >
+                    About client:
+                  </Heading>
+                </Box>
 
-            <Box width="380px" pad={{ bottom: 'medium' }}>
-              <Text
-                size="medium"
-                color="text-dark-grey"
-                style={
-                  ({ fontFamily: 'HelveticaNeueCyr' }, { lineHeight: '150%' })
-                }
-              >
-                With GapNurse, facilities know they’re covered during critical
-                moments and unexpected absences without breaking your budget.
-              </Text>
-            </Box>
+                <Box width="380px">
+                  <Text
+                    size="medium"
+                    color="text-dark-grey"
+                    style={
+                      ({ fontFamily: 'HelveticaNeueCyr' },
+                      { lineHeight: '150%' })
+                    }
+                  >
+                    With GapNurse, facilities know they’re covered during
+                    critical moments and unexpected absences without breaking
+                    your budget.
+                  </Text>
+                </Box>
+              </Box>
+            )}
           </Box>
           <Box align="center">
-            <Grid columns={{ count: columnsCounts, size: 'auto' }} gap="medium">
+            <Grid
+              columns={{ count: columnsCountTablet, size: 'auto' }}
+              gap="medium"
+            >
               <Box align="center">
                 <img
                   style={{ height: 'auto', width: '100%' }}
@@ -96,30 +95,113 @@ export const CaseGapNurse = () => {
               )}
             </Grid>
           </Box>
+          {isMobile && (
+            <Box align="center">
+              <Box>
+                <Heading level={4} color="brand" style={{ lineHeight: '130%' }}>
+                  About client:
+                </Heading>
+              </Box>
+              <Box width="380px">
+                <Text
+                  color="text-dark-grey"
+                  style={
+                    ({ fontFamily: 'HelveticaNeueCyr' }, { lineHeight: '150%' })
+                  }
+                >
+                  With GapNurse, facilities know they’re covered during critical
+                  moments and unexpected absences without breaking your budget.
+                </Text>
+              </Box>
+            </Box>
+          )}
         </Grid>
-        <Box align="center" margin={{ top: '70px' }}>
-          <Grid columns={['1/3', '1/3', '1/3']}>
-            <Box width="250px" align="center">
-              <Heading level={4} color="white">
+        <Box
+          width="100%"
+          height="150px"
+          align="center"
+          margin={isMobile ? { top: '30px' } : { top: '70px' }}
+          background={{ color: '#5E5999' }}
+        >
+          <Grid
+            columns={['1/3', '1/3', '1/3']}
+            style={
+              isMobile ? { width: '100%', height: '150px' } : { width: 'auto' }
+            }
+          >
+            <Box
+              style={
+                isMobile
+                  ? { width: 'auto', justifyContent: 'center' }
+                  : { width: '250px' }
+              }
+              align="center"
+              pad={{ horizontal: '2em' }}
+            >
+              <Heading
+                level={isMobile ? undefined : 4}
+                color="white"
+                margin={{ vertical: 'small' }}
+              >
                 Technologies:
               </Heading>
-              <Heading level={4} color="white" style={{ fontWeight: '700' }}>
+
+              <Heading
+                level={isMobile ? undefined : 4}
+                color="white"
+                style={{ fontWeight: '700', lineHeight: '16px' }}
+                margin={{ vertical: 'small' }}
+              >
                 React, Vue
               </Heading>
             </Box>
-            <Box width="250px" align="center">
-              <Heading level={4} color="white">
+            <Box
+              style={
+                isMobile
+                  ? { width: 'auto', justifyContent: 'center' }
+                  : { width: '250px' }
+              }
+              align="center"
+              pad={{ horizontal: '2em' }}
+            >
+              <Heading
+                level={isMobile ? undefined : 4}
+                color="white"
+                margin={{ vertical: 'small' }}
+              >
                 Languages:
               </Heading>
-              <Heading level={4} color="white" style={{ fontWeight: '700' }}>
+              <Heading
+                level={isMobile ? undefined : 4}
+                color="white"
+                style={{ fontWeight: '700', lineHeight: '16px' }}
+                margin={{ vertical: 'small' }}
+              >
                 JS
               </Heading>
             </Box>
-            <Box width="250px" align="center">
-              <Heading level={4} color="white">
+            <Box
+              style={
+                isMobile
+                  ? { width: 'auto', justifyContent: 'center' }
+                  : { width: '250px' }
+              }
+              align="center"
+              pad={{ horizontal: '2em' }}
+            >
+              <Heading
+                level={isMobile ? undefined : 4}
+                color="white"
+                margin={{ vertical: 'small' }}
+              >
                 Time:
               </Heading>
-              <Heading level={4} color="white" style={{ fontWeight: '700' }}>
+              <Heading
+                level={isMobile ? undefined : 4}
+                color="white"
+                style={{ fontWeight: '700', lineHeight: '16px' }}
+                margin={{ vertical: 'small' }}
+              >
                 124 h
               </Heading>
             </Box>
