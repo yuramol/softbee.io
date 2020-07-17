6eab3720b6cdb6840b1bcaa92518b660da820d10
Artem Marochkanych <serartmar@gmail.com>
2020-07-17 18:15:00 +0300

    Use Array.map to render team members

--- src/components/OurTeamAboutTeam/OurTeamAboutTeam.jsx
+++ src/components/OurTeamAboutTeam/OurTeamAboutTeam.jsx
@@ -4,6 +4,45 @@ import { useMediaQuery } from 'react-responsive';
 import { Heading } from '../../legos/typography/Heading';
 import { Text } from '../../legos/typography/Text';
 
+const teamMembers = [
+  {
+    name: 'Andriy Havriluk',
+    craft: 'programmer',
+    imageUrl: 'url(/assets/AndriyGavriluk.png)',
+  },
+  {
+    name: 'Artem Marochkanych',
+    craft: 'programmer',
+    imageUrl: 'url(/assets/ArtemMarochkanych.png)',
+  },
+  {
+    name: 'Vitaliy Hula',
+    craft: 'programmer',
+    imageUrl: 'url(/assets/VitaliyHula.png)',
+  },
+  {
+    name: 'Oleksandr Omelchenko',
+    craft: 'programmer',
+    imageUrl: 'url(/assets/OleksandrOmelchenko.png)',
+  },
+  {
+    name: 'Oleg Duma',
+    craft: 'programmer',
+    imageUrl: 'url(/assets/OlegDuma.png)',
+  },
+  {
+    name: 'Yuriy Moldavchuk',
+    craft: 'programmer',
+    imageUrl: 'url(/assets/YuriyMoldavchuk.png)',
+  },
+
+  {
+    name: 'Andriy Hula',
+    craft: 'designer',
+    imageUrl: 'url(/assets/YuriyMoldavchuk.png)',
+  },
+];
+
 export const OurTeamAboutTeam = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 900px)' });
   const columnsCount = isMobile ? 2 : 3;
@@ -25,7 +64,7 @@ export const OurTeamAboutTeam = () => {
           style={{ textAlign: 'center' }}
           margin={{ vertical: '15px' }}
         >
-          Real peaole creating real results
+          Real people creating real results
         </Heading>
       </Box>
       <Box align="center" width="100%">
@@ -54,335 +93,65 @@ export const OurTeamAboutTeam = () => {
             justify="center"
             justifyContent="around"
           >
-            <Box
-              style={
-                isMobile
-                  ? {
-                      width: '30vw',
-                      height: '30vw',
-                      minWidth: '150px',
-                      minHeight: '150px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-                  : {
-                      width: '17vw',
-                      height: '17vw',
-                      maxWidth: '320px',
-                      maxHeight: '320px',
-                      minWidth: '190px',
-                      minHeight: '190px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-              }
-              background={{
-                size: 'cover',
-                image: 'url(/assets/AndriyGavriluk.png)',
-              }}
-            >
+            {teamMembers.map(({ name, craft, imageUrl }) => (
               <Box
-                justify="end"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
+                key={name}
+                style={
+                  isMobile
+                    ? {
+                        width: '30vw',
+                        height: '30vw',
+                        minWidth: '150px',
+                        minHeight: '150px',
+                        boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
+                        borderRadius: '22px',
+                      }
+                    : {
+                        width: '17vw',
+                        height: '17vw',
+                        maxWidth: '320px',
+                        maxHeight: '320px',
+                        minWidth: '190px',
+                        minHeight: '190px',
+                        boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
+                        borderRadius: '22px',
+                      }
+                }
                 background={{
                   size: 'cover',
-                  image: 'url(/assets/Rectangle69.svg)',
+                  image: imageUrl,
                 }}
               >
-                <Box pad="small">
-                  <Heading
-                    level={5}
-                    style={isMobile ? { fontSize: '14px' } : undefined}
-                    color="brand"
-                    textAlign="bottom"
-                    margin={isMobile ? { vertical: '0' } : { vertical: '5px' }}
-                  >
-                    Andriy Gavriluk
-                  </Heading>
-                  <Text size={isMobile ? 'small' : 'medium'} color="brand">
-                    programmer
-                  </Text>
+                <Box
+                  justify="end"
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                  }}
+                  background={{
+                    size: 'cover',
+                    image: 'url(/assets/Rectangle69.svg)',
+                  }}
+                >
+                  <Box pad="small">
+                    <Heading
+                      level={5}
+                      color="brand"
+                      textAlign="bottom"
+                      margin={
+                        isMobile ? { vertical: '0' } : { vertical: '5px' }
+                      }
+                      style={isMobile ? { fontSize: '14px' } : undefined}
+                    >
+                      {name}
+                    </Heading>
+                    <Text fontSize={isMobile ? '10px' : '14px'} color="brand">
+                      {craft}
+                    </Text>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
-
-            <Box
-              style={
-                isMobile
-                  ? {
-                      width: '30vw',
-                      height: '30vw',
-                      minWidth: '150px',
-                      minHeight: '150px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-                  : {
-                      width: '17vw',
-                      height: '17vw',
-                      maxWidth: '320px',
-                      maxHeight: '320px',
-                      minWidth: '190px',
-                      minHeight: '190px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-              }
-              background={{
-                size: 'cover',
-                image: 'url(/assets/ArtemMarochkanych.png)',
-              }}
-            >
-              <Box
-                justify="end"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                background={{
-                  size: 'cover',
-                  image: 'url(/assets/Rectangle69.svg)',
-                }}
-              >
-                <Box pad="small">
-                  <Heading
-                    level={5}
-                    color="brand"
-                    textAlign="bottom"
-                    margin={isMobile ? { vertical: '0' } : { vertical: '5px' }}
-                    style={isMobile ? { fontSize: '14px' } : undefined}
-                  >
-                    Artem Marochkanych
-                  </Heading>
-                  <Text size={isMobile ? 'small' : 'medium'} color="brand">
-                    programmer
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
-            <Box
-              style={
-                isMobile
-                  ? {
-                      width: '30vw',
-                      height: '30vw',
-                      minWidth: '150px',
-                      minHeight: '150px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-                  : {
-                      width: '17vw',
-                      height: '17vw',
-                      maxWidth: '320px',
-                      maxHeight: '320px',
-                      minWidth: '190px',
-                      minHeight: '190px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-              }
-              background={{
-                size: 'cover',
-                image: 'url(/assets/OleksandrOmelchenko.png)',
-              }}
-            >
-              <Box
-                justify="end"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                background={{
-                  size: 'cover',
-                  image: 'url(/assets/Rectangle69.svg)',
-                }}
-              >
-                <Box pad="small">
-                  <Heading
-                    level={5}
-                    color="brand"
-                    textAlign="bottom"
-                    margin={isMobile ? { vertical: '0' } : { vertical: '5px' }}
-                    style={isMobile ? { fontSize: '14px' } : undefined}
-                  >
-                    Oleksandr Omelchenko
-                  </Heading>
-                  <Text size={isMobile ? 'small' : 'medium'} color="brand">
-                    programmer
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
-            <Box
-              style={
-                isMobile
-                  ? {
-                      width: '30vw',
-                      height: '30vw',
-                      minWidth: '150px',
-                      minHeight: '150px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-                  : {
-                      width: '17vw',
-                      height: '17vw',
-                      maxWidth: '320px',
-                      maxHeight: '320px',
-                      minWidth: '190px',
-                      minHeight: '190px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-              }
-              background={{
-                size: 'cover',
-                image: 'url(/assets/VitaliyHula.png)',
-              }}
-            >
-              <Box
-                justify="end"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                background={{
-                  size: 'cover',
-                  image: 'url(/assets/Rectangle69.svg)',
-                }}
-              >
-                <Box pad="small">
-                  <Heading
-                    level={5}
-                    color="brand"
-                    textAlign="bottom"
-                    margin={isMobile ? { vertical: '0' } : { vertical: '5px' }}
-                    style={isMobile ? { fontSize: '14px' } : undefined}
-                  >
-                    Vitaliy Hula
-                  </Heading>
-                  <Text size={isMobile ? 'small' : 'medium'} color="brand">
-                    programmer
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
-            <Box
-              style={
-                isMobile
-                  ? {
-                      width: '30vw',
-                      height: '30vw',
-                      minWidth: '150px',
-                      minHeight: '150px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-                  : {
-                      width: '17vw',
-                      height: '17vw',
-                      maxWidth: '320px',
-                      maxHeight: '320px',
-                      minWidth: '190px',
-                      minHeight: '190px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-              }
-              background={{
-                size: 'cover',
-                image: 'url(/assets/MoldavchukYurij.png)',
-              }}
-            >
-              <Box
-                justify="end"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                background={{
-                  size: 'cover',
-                  image: 'url(/assets/Rectangle69.svg)',
-                }}
-              >
-                <Box pad="small">
-                  <Heading
-                    level={5}
-                    color="brand"
-                    textAlign="bottom"
-                    margin={isMobile ? { vertical: '0' } : { vertical: '5px' }}
-                    style={isMobile ? { fontSize: '14px' } : undefined}
-                  >
-                    Moldavchuk Yurij
-                  </Heading>
-                  <Text size={isMobile ? 'small' : 'medium'} color="brand">
-                    programmer
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
-            <Box
-              style={
-                isMobile
-                  ? {
-                      width: '30vw',
-                      height: '30vw',
-                      minWidth: '150px',
-                      minHeight: '150px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-                  : {
-                      width: '17vw',
-                      height: '17vw',
-                      maxWidth: '320px',
-                      maxHeight: '320px',
-                      minWidth: '190px',
-                      minHeight: '190px',
-                      boxShadow: ' 0px 0px 22px rgba(200,200,200,0.3)',
-                      borderRadius: '22px',
-                    }
-              }
-              background={{
-                size: 'cover',
-                image: 'url(/assets/OlegDuma.png)',
-              }}
-            >
-              <Box
-                justify="end"
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                background={{
-                  size: 'cover',
-                  image: 'url(/assets/Rectangle69.svg)',
-                }}
-              >
-                <Box pad="small">
-                  <Heading
-                    level={5}
-                    color="brand"
-                    textAlign="bottom"
-                    margin={isMobile ? { vertical: '0' } : { vertical: '5px' }}
-                    style={
-                      isMobile
-                        ? { fontSize: '14px', lineHeight: '22px' }
-                        : { lineHeight: '22px' }
-                    }
-                  >
-                    Oleg Duma
-                  </Heading>
-                  <Text size={isMobile ? 'small' : 'medium'} color="brand">
-                    programmer
-                  </Text>
-                </Box>
-              </Box>
-            </Box>
+            ))}
           </Grid>
         </Box>
       </Box>
