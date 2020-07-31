364eb1b8a269ea0a26c21873544ccb7f7f42f07e
yuramol86 <yuramol86@gmail.com>
2020-07-31 23:44:19 +0300

    Change styles for Heading and for article body

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,10 +2,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import { Box, Image } from 'grommet';
 
 import { Layout } from '../components/Layout';
 import { SEO } from '../components/SEO';
+
 import { RouterLink } from '../legos/RouterLink';
+import { SiteFooter } from '../components/Footer';
+import { SiteHeader } from '../components/Header';
+import { Heading } from '../legos/typography/Heading';
 
 const BlogPostTemplate = ({ data, pageContext, location }) => {
   const post = data.mdx;
@@ -14,45 +19,64 @@ const BlogPostTemplate = ({ data, pageContext, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
+      <SiteHeader />
       <SEO
         title={post.frontmatter.title}
         description={post.frontmatter.description || post.excerpt}
       />
-      <h1>{post.frontmatter.title}</h1>
-      <p
-        style={{
-          display: `block`,
-        }}
-      >
-        {post.frontmatter.date}
-      </p>
-      <MDXRenderer>{post.body}</MDXRenderer>
-      <hr />
+      <Box margin={{ horizontal: 'xlarge' }} pad="xlarge">
+        <Box
+          fill
+          margin={{ horizontal: 'xlarge' }}
+          pad={{ horizontal: 'xlarge' }}
+          justify="center"
+          align="center"
+        >
+          <Image
+            fill="horizontal"
+            src="/assets/postimage.svg"
+            alt="On laptop open code editor"
+          />
 
-      <ul
-        style={{
-          display: `flex`,
-          flexWrap: `wrap`,
-          justifyContent: `space-between`,
-          listStyle: `none`,
-          padding: 0,
-        }}
-      >
-        <li>
-          {previous && (
-            <RouterLink to={`blog${previous.fields.slug}`} rel="prev">
-              ← {previous.frontmatter.title}
-            </RouterLink>
-          )}
-        </li>
-        <li>
-          {next && (
-            <RouterLink to={`blog${next.fields.slug}`} rel="next">
-              {next.frontmatter.title} →
-            </RouterLink>
-          )}
-        </li>
-      </ul>
+          <Heading
+            level={2}
+            color="brand"
+            textAlign="center"
+            fontWeight="normal"
+          >
+            {post.frontmatter.title}
+          </Heading>
+
+          <MDXRenderer>{post.body}</MDXRenderer>
+
+          <ul
+            style={{
+              display: `flex`,
+              flexWrap: `wrap`,
+              justifyContent: `space-between`,
+              listStyle: `none`,
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <RouterLink to={`blog${previous.fields.slug}`} rel="prev">
+                  ← {previous.frontmatter.title}
+                </RouterLink>
+              )}
+            </li>
+            <li>
+              {next && (
+                <RouterLink to={`blog${next.fields.slug}`} rel="next">
+                  {next.frontmatter.title} →
+                </RouterLink>
+              )}
+            </li>
+          </ul>
+        </Box>
+      </Box>
+
+      <SiteFooter />
     </Layout>
   );
 };
