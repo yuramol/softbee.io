3a0c85c3900c9a5e46c11ece94b38b5523900f27
Artem Marochkanych <serartmar@gmail.com>
2021-12-10 16:18:35 +0200

    Sort imports in components/CaseGapNurse/styled.js

--- src/components/CaseGapNurse/styled.js
+++ src/components/CaseGapNurse/styled.js
@@ -1,6 +1,5 @@
-import styled from 'styled-components';
-
 import { Grid } from 'grommet';
+import styled from 'styled-components';
 
 export const GridInfo = styled(Grid)`
   width: ${({ isMobile }) => (isMobile ? '100%' : undefined)};
