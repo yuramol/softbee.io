7753c748b8c05b475bdfc8d08c4668238ec3783b
Олександр Заставний <alex.roox5@gmail.com>
2021-12-14 13:14:41 +0200

    fix Safari bug with prelaoder, delete check fonts load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,28 +1,10 @@
 require('./static/assets/css/preloader.css');
 
 export const onInitialClientRender = () => {
-  let checkLoad = 0;
-  const hide = () => {
-    checkLoad += 1;
-    if (checkLoad === 2) {
-      document.body.classList.add('loaded_hiding');
-      document.body.classList.remove('not-loaded');
-      setTimeout(() => {
-        document.body.classList.remove('loaded_hiding');
-        document.getElementById('___loader').remove();
-      }, 1000);
-    }
-  };
-  const checkFontName = arr =>
-    arr.find(
-      font => font.family.replace(/[^a-zA-Z]+/g, '') === 'HelveticaNeueCyr', // some browsers pass values /"HelveticaNeueCyr/" then we delete unnecessary
-    );
-  document.fonts.onloadingdone = function(fontFaceSetEvent) {
-    if (checkFontName(fontFaceSetEvent.fontfaces)) {
-      hide();
-    }
-  };
-  document.fonts.ready.then(function() {
-    hide();
-  });
+  document.body.classList.add('loaded_hiding');
+  document.body.classList.remove('not-loaded');
+  setTimeout(() => {
+    document.body.classList.remove('loaded_hiding');
+    document.getElementById('___loader').remove();
+  }, 1000);
 };
