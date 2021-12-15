42c053e21f2dd91d148d47f9b6d0cdd83f6d8127
Artem Marochkanych <serartmar@gmail.com>
2021-12-15 17:04:41 +0200

    Make the loader work again

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,10 @@
 require('./static/assets/css/preloader.css');
 
 export const onInitialClientRender = () => {
-  document.body.classList.add('loaded_hiding');
-  document.body.classList.remove('not-loaded');
+  setTimeout(() => {
+    document.body.classList.add('loaded_hiding');
+    document.body.classList.remove('not-loaded');
+  }, 700);
   setTimeout(() => {
     document.body.classList.remove('loaded_hiding');
     document.getElementById('___loader').remove();
