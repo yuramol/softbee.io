72bd206e7c655090aceb959ea789fd5a148957af
Artem Marochkanych <serartmar@gmail.com>
2022-01-04 09:19:28 +0200

    Remove needless custom scrollbar

--- src/utils/globalStyles.js
+++ src/utils/globalStyles.js
@@ -1,30 +1,4 @@
-import { createGlobalStyle, css } from 'styled-components';
-import { theme } from './theme';
-
-export const GlobalStyle = createGlobalStyle`
-
-   body {
-
-      &::-webkit-scrollbar {
-         background-color: #fff;
-         width: 16px; 
-      }
-
-      &::-webkit-scrollbar-track {
-         background-color: #fff;
-      }
-
-      &::-webkit-scrollbar-thumb {
-         background-color: ${theme.global.colors.brand};
-         border-radius: 16px;
-         border: 4px solid #fff;
-      }
-
-      &::-webkit-scrollbar-button {
-         display:none;
-      }
-   }
-`;
+import { css } from 'styled-components';
 
 export const hoveredLink = color =>
   css`
