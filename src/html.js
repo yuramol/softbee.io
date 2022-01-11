ce4c918097f026b4043b07d1b21390fddbee88cc
unknown <sharkkizayk@gmail.com>
2022-01-11 14:46:24 +0200

    delete console.log() in html.js

--- src/html.js
+++ src/html.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { PreloaderLogo } from './legos/PreloaderLogo';
 
 export default function HTML(props) {
-    console.log(props)
   return (
     <html {...props.htmlAttributes}>
       <head>
