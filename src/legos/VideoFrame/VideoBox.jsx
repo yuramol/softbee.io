7e1966de59137f8f6b85d04ba8597796d0e8c630
Станіслав Бабич <themissingarchi@gmail.com>
2022-02-17 16:53:15 +0200

    feat: add video wraper wor work case

---
+++ src/legos/VideoFrame/VideoBox.jsx
@@ -0,0 +1,8 @@
+import { Box } from 'grommet';
+import styled from 'styled-components';
+
+export const VideoBox = styled(Box)`
+  position: relative;
+  width: 100%;
+  padding-top: calc(9 / 16 * 100%);
+`;
