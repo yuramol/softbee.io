0bf7586350cdcc1381b0e854193b64762e1d3333
Mikhail Yastremsky <maukul06@gmail.com>
2023-05-10 11:29:49 +0300

    fix: send to slack

--- src/utils/useForm.js
+++ src/utils/useForm.js
@@ -1,13 +1,7 @@
-const encode = data => {
-  return Object.keys(data)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-    .join('&');
-};
+import { sendSlack } from './useSlack';
 
 export const sendForm = (formName, input) => {
-  return fetch('/', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: encode({ 'form-name': formName, ...input }),
-  });
+  sendSlack(
+    ` Name: ${input.name}\n Website: ${input.website}\n Email: ${input.email}\n Comment: ${input.comment}\n`,
+  );
 };
