7d46eb9bb7d7592a473224938fc9acd71b1af5a3
Mikhail Yastremsky <maukul06@gmail.com>
2023-05-10 11:44:44 +0300

    fix: rewrite send to netlify

--- src/utils/useForm.js
+++ src/utils/useForm.js
@@ -1,7 +1,18 @@
 import { sendSlack } from './useSlack';
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&');
+};
+
 export const sendForm = (formName, input) => {
   sendSlack(
-    ` Name: ${input.name}\n Website: ${input.website}\n Email: ${input.email}\n Comment: ${input.comment}\n`,
+    `Name: ${input.name}\n Website: ${input.website}\n Email: ${input.email}\n Comment: ${input.comment}\n`,
   );
+  return fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode({ 'form-name': formName, ...input }),
+  });
 };
